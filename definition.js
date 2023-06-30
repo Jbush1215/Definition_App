b2d408f90623f0e2cd1deaee5613de6171038a64
Jbush1215 <jessierbush1215@gmail.com>
2023-06-30 18:28:05 -0400

    Error checking was added to submissions

--- definition.js
+++ definition.js
@@ -1,11 +1,24 @@
 const form = document.querySelector(".js-definition-form");
 let wordInputField = document.querySelector('[name="word"]');
+let errorField = document.querySelector['js-error'];
+
+function fetchDefinition(word) {
+    
+}
 
 function formSubmitted(event) {
     event.preventDefault();
-    alert(wordInputField.value)
 
-    wordInputField.value = "";
+    let word = wordInputField.trim();
+
+    if (word.length > 0) {
+        errorField.innerText = '';
+        wordInputField.value = "";
+        fetchDefinition(word);
+    }
+    else {
+        errorField.innerText = 'Enter a word for us to describe. You may have entered a word that is not in the API Dictionary.';
+    }
 }
 
 form.addEventListener('submit', formSubmitted);
