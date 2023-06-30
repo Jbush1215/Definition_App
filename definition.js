14ef15e504ec8eb45c7107483fe6f253b796f9de
Jbush1215 <jessierbush1215@gmail.com>
2023-06-30 19:19:35 -0400

    fixed innerText error with syntax, added undefined check on results, and a few text changes.

--- definition.js
+++ definition.js
@@ -1,23 +1,42 @@
 const form = document.querySelector(".js-definition-form");
 let wordInputField = document.querySelector('[name="word"]');
-let errorField = document.querySelector['js-error'];
+let errorField = document.querySelector('.js-error');
 
-function fetchDefinition(word) {
-    
+function getDefinitionEndpoint(wordToDefine) {
+    return `https://api.dictionaryapi.dev/api/v2/entries/en/${wordToDefine}`;
+}
+
+function renderDefinition(definition) { }
+
+function fetchDefinition(wordToDefine) {
+    fetch(getDefinitionEndpoint(wordToDefine))
+        .then((x) => x.json())
+        .then(response => {
+            let results = response.results;
+            if (typeof results !== 'undefined' && results.length > 0) {
+                let definition = results[0];
+            }
+            else {
+                errorField.innerText = `The word ${wordToDefine} does not exist.`;
+            }
+            renderDefinition(response);
+            console.log(wordToDefine);
+        })
 }
 
 function formSubmitted(event) {
     event.preventDefault();
 
-    let word = wordInputField.trim();
+    let word = wordInputField.value.trim();
 
     if (word.length > 0) {
-        errorField.innerText = '';
+        errorField.innerText = ``;
         wordInputField.value = "";
         fetchDefinition(word);
     }
     else {
-        errorField.innerText = 'Enter a word for us to describe. You may have entered a word that is not in the API Dictionary.';
+        errorField.innerText = `Enter a word for us to describe. 
+        You may have entered a word that is not in the API Dictionary.`;
     }
 }
 
