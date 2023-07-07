754b88523d4a1e6756b37414bbdaef51c6274e92
Jbush1215 <jessierbush1215@gmail.com>
2023-07-07 16:50:31 -0400

    added README.md and progressed on function renderDefinition

---
+++ README.md
@@ -0,0 +1,10 @@
+# QuickDef App
+
+
+This app is meant to be used as my project submission for my coding bootcamp. Enter a word into the search bar. It grabs a definition from an API and then displays the meanings of the word you wished to define. See below for API details.
+
+## Resources Used
+
+The API I used can be found here: [https://dictionaryapi.dev/](https://dictionaryapi.dev/)
+
+## User Stories
--- definition.js
+++ definition.js
@@ -1,16 +1,42 @@
 const form = document.querySelector(".js-definition-form");
 let wordInputField = document.querySelector('[name="word"]');
 let errorField = document.querySelector('.js-error');
-let definitionSection = document.querySelector('.js-definition-data')
+let headingSection = document.querySelector('.js-heading-data');
+let nounMeaningSection = document.querySelector('.js-noun-meaning-data')
+let verbsMeaningSection = document.querySelector('.js-verbs-meaning-data')
 
 function getDefinitionEndpoint(wordToDefine) {
     return `https://api.dictionaryapi.dev/api/v2/entries/en/${wordToDefine}`;
 }
 
 function renderDefinition(definition) {
-    definitionSection.innerHTML = `
-        <h2>${definition}</h2>
+    let meanings = definition.meanings;
+    let nouns = meanings[0];
+    let verbs = meanings[1];
+    let nounDefinitions = nouns.definitions;
+    let verbsDefinitions = verbs.definitions;
+
+    headingSection.innerHTML = `
+        <h2>${definition.word}</h2>
     `;
+    nounMeaningSection.innerHTML = `
+            <p>Noun Meaning definitions:</p>
+            <ul>
+        `;
+    
+    if (Array.isArray(nounDefinitions)) {
+        for (let def of nounDefinitions)
+            nounMeaningSection.innerHTML += `
+           <li>${nounDefinitions[def]}</li>
+        `;
+    }
+    else {
+        nounMeaningSection.innerHTML += '<p>Error Rending nounMenaingSection</p>'
+    };
+    nounMeaningSection.innerHTML += `
+            </ul>
+        `;
+    console.log(verbsDefinitions) /* Todo */
  }
 
 function fetchDefinition(wordToDefine) {
@@ -18,13 +44,13 @@ function fetchDefinition(wordToDefine) {
         .then(x => x.json())
         .then(results => {
             if (typeof results !== 'undefined' && results.length > 0) {
-                let definition = results[0];
+                definition = results[0];
             }
             else {
                 errorField.innerText = `The word ${wordToDefine} does not exist in this Dictionary API.`;
             }
-            renderDefinition(results);
-            console.log(wordToDefine);
+            renderDefinition(definition);
+            console.log(definition);
         })
 }
 
@@ -35,7 +61,7 @@ function formSubmitted(event) {
 
     if (word.length > 0) {
         errorField.innerText = ``;
-        wordInputField.value = "";
+        wordInputField.value = ``;
         fetchDefinition(word);
     }
     else {
